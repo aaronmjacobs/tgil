fff4fd34a631d07ddb9f1c8df1520b1ab9db2dfe
Aaron Jacobs <aaron@aaronmjacobs.com>
2014-12-26 19:32:51 -0800

    Set up main loop

--- source/main.cpp
+++ source/main.cpp
@@ -2,11 +2,22 @@
 #include "GLIncludes.h"
 #include "LogHelper.h"
 
+#include <glm/glm.hpp>
+
 #include <cstdlib>
+
+namespace {
+
+void errorCallback(int error, const char* description) {
+   LOG_FATAL("Error " << error << ": " << description, "GLFW Error");
+}
+
+}
 
 int main(int argc, char *argv[]) {
    LOG_INFO(VERSION_TYPE << " " << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_MICRO << "." << VERSION_BUILD, PROJECT_NAME);
 
+   glfwSetErrorCallback(errorCallback);
    int glfwInitRes = glfwInit();
    if (!glfwInitRes) {
       LOG_FATAL("Unable to initialize GLFW", "GLFW Error");
@@ -18,24 +29,40 @@ int main(int argc, char *argv[]) {
    }
 
    glfwMakeContextCurrent(window);
+   glfwSwapInterval(1); // VSYNC
 
-   // Load glad
    int gladInitRes = gladLoadGL();
    if (!gladInitRes) {
       LOG_FATAL("Unable to initialize glad", "glad Error");
    }
 
-   // Game loop
+   // Timing values
+   double lastTime = glfwGetTime();
+   const double dt = 1.0 / 60.0;
+   double accumulator = dt;
+
    while (!glfwWindowShouldClose(window)) {
-      // TODO Tick
+      // Calculate the frame time
+      double now = glfwGetTime();
+      double frameTime = glm::min(now - lastTime, 0.25); // Cap the frame time to .25 seconds to prevent spiraling
+      lastTime = now;
+
+      accumulator += frameTime;
+      while (accumulator >= dt) {
+         // TODO tick
+
+         accumulator -= dt;
+      }
 
       // TODO render
 
       glfwSwapBuffers(window);
 
-      // Poll for events
       glfwPollEvents();
    }
+
+   glfwDestroyWindow(window);
+   glfwTerminate();
 
    return EXIT_SUCCESS;
 }
