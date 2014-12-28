f6500e938ea444731965b91499ca4e0ebfadaa44
Aaron Jacobs <aaron@aaronmjacobs.com>
2014-12-27 17:54:45 -0800

    Add initial shaders

---
+++ data/shaders/phong_frag.glsl
@@ -0,0 +1,64 @@
+#version 120
+
+#define MAX_LIGHTS 10
+
+struct Light {
+  vec3 position, color;
+  float constFalloff, linearFalloff, squareFalloff;
+};
+
+struct Material {
+  vec3 ambient, diffuse, specular, emission;
+  float shininess;
+};
+
+uniform Light uLights[MAX_LIGHTS];
+uniform int uNumLights;
+uniform Material uMaterial;
+uniform vec3 uCameraPos;
+
+varying vec3 vWorldPosition;
+varying vec3 vNormal;
+
+//out vec4 color;
+
+void main() {
+   vec3 lNormal = normalize(vNormal);
+
+   vec3 finalColor = vec3(0.0, 0.0, 0.0);
+   for (int i = 0; i < uNumLights; ++i) {
+      // Diffuse light
+      vec3 toLight = uLights[i].position - vWorldPosition;
+      float lightDistance = length(toLight);
+      toLight = normalize(toLight);
+      float nDotL = dot(lNormal, toLight);
+      float diffuseAmount = max(nDotL, 0.0);
+
+      // Specular light
+      float specularAmount = 0.0;
+      if (nDotL >= 0.0) {
+         vec3 toCamera = normalize(uCameraPos - vWorldPosition);
+         vec3 reflection = normalize(2.0 * nDotL * lNormal - toLight);
+         specularAmount = pow(max(dot(toCamera, reflection), 0.0), uMaterial.shininess);
+      }
+
+      // Falloff
+      float falloff;
+      if (uLights[i].linearFalloff == 0.0) {
+        falloff = 1.0;
+      } else {
+        falloff = 1.0 / (uLights[i].constFalloff
+                    + uLights[i].linearFalloff * lightDistance
+                    + uLights[i].squareFalloff * lightDistance * lightDistance);
+      }
+
+      finalColor += (uMaterial.diffuse * diffuseAmount
+                  + uMaterial.specular * specularAmount)
+                  * falloff * uLights[i].color;
+   }
+
+   finalColor += uMaterial.ambient;
+   finalColor += uMaterial.emission;
+
+   gl_FragColor = vec4(finalColor.rgb, 1);
+}
---
+++ data/shaders/phong_vert.glsl
@@ -0,0 +1,24 @@
+#version 120
+
+uniform mat4 uProjMatrix;
+uniform mat4 uViewMatrix;
+uniform mat4 uModelMatrix;
+uniform mat4 uNormalMatrix;
+
+attribute vec3 aPosition;
+attribute vec3 aNormal;
+
+varying vec3 vWorldPosition;
+varying vec3 vNormal;
+
+void main() {
+   // Transforms
+   vec4 lPosition = uModelMatrix * vec4(aPosition.xyz, 1.0);
+   vWorldPosition = lPosition.xyz;
+   gl_Position = uProjMatrix * uViewMatrix * lPosition;
+
+   // Calculate the relative normal
+   vec4 lNormal = vec4(aNormal.xyz, 0.0);
+   lNormal = uNormalMatrix * lNormal;
+   vNormal = lNormal.xyz;
+}
