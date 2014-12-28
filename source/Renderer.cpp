4899a86a00e6ae5c9c186600d821282689b598ed
Aaron Jacobs <aaron@aaronmjacobs.com>
2014-12-28 00:10:46 -0800

    Add OpenGL error check on render

--- source/Renderer.cpp
+++ source/Renderer.cpp
@@ -1,5 +1,44 @@
 #include "GLIncludes.h"
+#include "LogHelper.h"
 #include "Renderer.h"
+
+#include <string>
+
+namespace {
+
+std::string getErrorName(GLenum error) {
+   switch (error) {
+      case GL_NO_ERROR:
+         return "GL_NO_ERROR";
+      case GL_INVALID_ENUM:
+         return "GL_INVALID_ENUM";
+      case GL_INVALID_VALUE:
+         return "GL_INVALID_VALUE";
+      case GL_INVALID_OPERATION:
+         return "GL_INVALID_OPERATION";
+      case GL_INVALID_FRAMEBUFFER_OPERATION:
+         return "GL_INVALID_FRAMEBUFFER_OPERATION";
+      case GL_OUT_OF_MEMORY:
+         return "GL_OUT_OF_MEMORY";
+      case GL_STACK_UNDERFLOW:
+         return "GL_STACK_UNDERFLOW";
+      case GL_STACK_OVERFLOW:
+         return "GL_STACK_OVERFLOW";
+      case GL_TABLE_TOO_LARGE:
+         return "GL_TABLE_TOO_LARGE";
+      default:
+         return "Unknown";
+   }
+}
+
+void checkGLError() {
+   GLenum error = glGetError();
+   if (error != GL_NO_ERROR) {
+      LOG_WARNING("OpenGL error " << error << " (" << getErrorName(error) << ")");
+   }
+}
+
+} // namespace
 
 Renderer::Renderer() {
 }
@@ -20,5 +59,7 @@ void Renderer::prepare() {
 }
 
 void Renderer::render() {
+   checkGLError();
+
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
