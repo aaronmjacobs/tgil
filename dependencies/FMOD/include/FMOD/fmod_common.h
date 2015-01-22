d0c69e0b7adbe2f949df5f7f04f610faec039646
Aaron Jacobs <aaron@aaronmjacobs.com>
2015-01-21 23:43:04 -0800

    Updated fmod libs on OS X

--- dependencies/FMOD/include/FMOD/fmod_common.h
+++ dependencies/FMOD/include/FMOD/fmod_common.h
@@ -1,7 +1,7 @@
 /*$ preserve start $*/
 
 /* ================================================================================================== */
-/* FMOD Studio - Common C/C++ header file. Copyright (c), Firelight Technologies Pty, Ltd. 2004-2014. */
+/* FMOD Studio - Common C/C++ header file. Copyright (c), Firelight Technologies Pty, Ltd. 2004-2015. */
 /*                                                                                                    */
 /* This header is included by fmod.hpp (C++ interface) and fmod.h (C interface) therefore is the      */
 /* base header for all FMOD headers.                                                                  */
@@ -15,7 +15,7 @@
     0xaaaabbcc -> aaaa = major version number.  bb = minor version number.  cc = development version number.
 */
 
-#define FMOD_VERSION    0x00010509
+#define FMOD_VERSION    0x00010510
 
 /*
     Compiler specific settings.
